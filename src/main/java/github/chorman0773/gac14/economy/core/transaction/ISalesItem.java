5c7c20b6b8e6ec16417bfde83419f16cfaad1eda
Connor Horman <chorman64@gmail.com>
2019-05-06 20:51:20 -0400

    Add API Documentation for Eco Core

--- src/main/java/github/chorman0773/gac14/economy/core/transaction/ISalesItem.java
+++ src/main/java/github/chorman0773/gac14/economy/core/transaction/ISalesItem.java
@@ -4,28 +4,74 @@ import java.util.UUID;
 
 import net.minecraft.util.ResourceLocation;
 
+/**
+ * An ISalesItem refers to something that is being sold.
+ * It is possible to create a transaction 
+ * @author chorm
+ */
 public interface ISalesItem {
 	
-	
+	/**
+	 * @return the id of the seller, usually the NIL ID for the Server.
+	 */
 	UUID getSeller();
 	
+	/**
+	 * Obtains the cost of some amount of an item
+	 * @param amount the amount being requested
+	 * @return returns the cost for that amount, or Double.NEGATIVE_INFINITY if the requested amount is unavailable.
+	 */
 	double getCost(int amount);
 	
+	/**
+	 * Creates a transaction for a particular buyer, where the Seller of the Transaction is getSeller(), and the buyer is buyer.
+	 * @param buyer The buyer for the transaction
+	 * @param amount the quantity of the item being purchased
+	 * @return A new transaction, which causes amount of the item to be transfered to the buyer, in exchange for getCost(amount) dollars, or null if the requested amount is not available.  
+	 */
 	Transaction createTransaction(UUID buyer, int amount);
 	
-	ResourceLocation getName();
+	/**
+	 * Obtains the name of Item being sold.
+	 * If the item is in the minecraft domain, then it must be a vanilla item, or a loot table.
+	 * If the item is in the gac14 domain, then it must be a loot table, or an extension item.
+	 * Otherwise the type of item being sold is implementation defined.
+	 * @return The name of the item being sold
+	 */
+	ResourceLocation getItemName();
 	
+	/**
+	 * Obtains the name of the provider for the transaction.
+	 * @see Transaction#getProviderName()
+	 */
+	ResourceLocation getProviderName();
+	
+	/**
+	 * An Equivalence is mapping between a Source Item, which does not have a corresponding ISalesItem and a Resultant Item, which does.
+	 * Despite the names, this mapping is reflexive, that is, either item may be used in-place of the other.
+	 * There is also a quantity modifier, which defines the quantity
+	 *   of the resultant item which corresponds to 1 of the source item.
+	 * @author chorm
+	 */
 	public static interface Equivalence{ 
 		
 		/**
+		 * Obtains the name of Item which does not have a corresponding ISalesItem
+		 * If the item is in the minecraft domain, then it must be a vanilla item, or a loot table.
+		 * If the item is in the gac14 domain, then it must be a loot table, or an extension item.
+		 * Otherwise the type of item is implementation defined.
 		 * @return the name of the source item
 		 */
-		ResourceLocation getFromName();
+		ResourceLocation getSourceName();
 		
 		/**
+		 * Obtains the name of Item which does have a corresponding ISalesItem
+		 * If the item is in the minecraft domain, then it must be a vanilla item, or a loot table.
+		 * If the item is in the gac14 domain, then it must be a loot table, or an extension item.
+		 * Otherwise the type of item is implementation defined.
 		 * @return the name of the resultant item
 		 */
-		ResourceLocation getToName();
+		ResourceLocation getResultName();
 		
 		/**
 		 * @return the quantity of the resultant item represented by the source item
@@ -35,8 +81,21 @@ public interface ISalesItem {
 		
 	};
 	
+	/**
+	 * Gets the minimum advertised quantity. 
+	 * This value must be less-than or equal-to the maximum advertised quantity.
+	 * Any attempts to request a quantity that is less-than this value MAY or MAY NOT be honored (this is implementation-defined).
+	 * If the request is not honored, then the appropriate error value SHOULD be returned.
+	 * @return The minimum quantity
+	 */
 	int getMinQuantity();
 	
+	/**
+	 * Gets the maximum advertised quantity. 
+	 * Any attempts to request a quantity that is greater-than this value SHOULD NOT be honored.
+	 * If the request is not honored, then the appropriate error value SHOULD be returned.
+	 * @return The maximum quantity
+	 */
 	int getMaxQuantity();
 	
 	
--- src/main/java/github/chorman0773/gac14/economy/core/transaction/Transaction.java
+++ src/main/java/github/chorman0773/gac14/economy/core/transaction/Transaction.java
@@ -4,25 +4,71 @@ import java.util.UUID;
 
 import org.apache.logging.log4j.core.util.UuidUtil;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 
+/**
+ * A Transaction refers to an exchange between 2 players or a player and the server.
+ * A Transaction has 4 phases: Initialization, Start, Completion, and Finalization.
+ * The Exchange occurs during Completion. 
+ * @author chorm
+ */
 public abstract class Transaction {
 
-	
+	/**
+	 * Called During Phase 1 of the Transaction
+	 * Any resources should be acquired during this phase.
+	 */
 	public final void init() {
 		if(MinecraftForge.EVENT_BUS.post(new TransactionEvent.Initialize(this))) 
 			doInit();
 		
 	}
 	
+	/**
+	 * Obtains the name of the Provider which created the Transaction.
+	 * The Provider name is used to identify the kind of transaction without knowing the source of the transaction.
+	 * <br/><br/>
+	 * There is a list of Standard Providers in the gac14 domain, no other providers may be registered in this domain
+	 * <ul>
+	 * <li>gac14:eco/exchange -&gt; The Admin Shop using the exchange</li>
+	 * <li>gac14:eco/auction -&gt; The Auction House</li>
+	 * <li>gac14:eco/admin_shop -&gt; The Admin Shop using fixed prices</li>
+	 * <li>gac14:eco/player_shop -&gt; A Player Shop</li>
+	 * <li>gac14:eco/trade -&gt; The Trade System</li>
+	 * </ul>
+	 * <br/>
+	 * See Gac14 Specification for details about these providers
+	 * @return The name of the Provider
+	 */
+	public abstract ResourceLocation getProviderName();
+	
+	/**
+	 * Obtains the Principal ID of the Seller, or the originator of the transaction.
+	 * The Server is identified by the NIL UUID
+	 * Who is the seller in a transaction is defined by the provider.
+	 * @return The UUID of the Seller
+	 */
 	public abstract UUID getSeller();
 	
+	/**
+	 * Obtains the Principal ID of the Reciever.
+	 * Who the Reciever is in a transaction is defined by the provider.
+	 * @return
+	 */
 	public abstract UUID getReciever();
 	
+	/**
+	 * Obtains the ID of the Transaction.
+	 * @return The Transaction ID.
+	 */
 	public final UUID getTransId() {
 		return TransactionId;
 	};
 	
+	/**
+	 * Starts the Transaction, and checks if the participants (Seller and Reciever) have the resources available to complete.
+	 */
 	public final void start() {
 		
 		TransactionEvent.Start evStart = new TransactionEvent.Start(this);
@@ -41,6 +87,9 @@ public abstract class Transaction {
 		}
 	};
 	
+	/**
+	 * Completes the Transaction and transfers the resources between the participants.
+	 */
 	public final void complete() {
 		
 		TransactionEvent.Complete evComplete = new TransactionEvent.Complete(this);
@@ -59,6 +108,9 @@ public abstract class Transaction {
 		}
 	};
 	
+	/**
+	 * Finalizes the Transaction between 2 participants.
+	 */
 	 public final void finish() {
 		MinecraftForge.EVENT_BUS.post(new TransactionEvent.Finish(this));
 		doFinish();
